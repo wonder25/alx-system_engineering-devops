095ee9aaad02296dd41ffcf3ad142989c9afd88a
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-04 17:04:55 +0300

    install nginx using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# install nginx using puppet
+
+package { 'nginx':
+  ensure  => installed,
+}
+
+file_line { 'aaaa':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.google.com;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
