71509d2a9ff4687bba7c6cb9e53eda6b43ed4d0d
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-04 17:52:36 +0300

    configures ssh via puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# client configurations with puppet
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
