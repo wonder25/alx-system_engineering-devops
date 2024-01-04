4c685ce2e926d3de6092b724bab60399b14a4639
wonder25 <wonderfulmwinzi14@gmail.com>
2024-01-04 17:13:32 +0300

    install nginx via puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,7 +9,7 @@ file_line { 'aaaa':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.google.com;',
+  line    => 'rewrite ^/redirect_me https://www.google.com permanent;',
 }
 
 file { '/var/www/html/index.html':
